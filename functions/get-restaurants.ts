0d409fad8f61b4a74411a322128df2b2e7e8deb9
Abhay Rajan <arajan@AJ-MacBook-Pro.attlocal.net>
2020-04-20 08:19:09 -0500

    Update get-restaurants lambda

--- functions/get-restaurants.ts
+++ functions/get-restaurants.ts
@@ -2,7 +2,18 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 import * as AWS from "aws-sdk";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const defaultResultCount = process.env.defaultResultCount || 8;
+const defaultResultCount = Number(process.env.defaultResultCount) || 8;
+const restaurantsTable = process.env.restaurantsTable;
+
+const getRestaurants = async (numResults: number) => {
+  let req = {
+    TableName: restaurantsTable,
+    Limit: numResults,
+  };
+
+  let response = await dynamoDb.scan(req).promise();
+  return response.Items;
+};
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
   let restaurants = getRestaurants(defaultResultCount);
